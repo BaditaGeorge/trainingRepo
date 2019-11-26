463be798f0a24a7385f155cd1a423a5a954aeeec
BaditaGeorge <baditageorge@yahoo.com>
2019-11-26 18:02:19 +0200

    assign1

--- asign1/e1.js
+++ asign1/e1.js
@@ -2,20 +2,20 @@
 
 // }
 // createAtMouse();
-function main(){
-    let posX=-1,posY=-1;
+function main() {
+    let posX = -1, posY = -1;
     let arr = new Array(1000);
     let poses = [];
     let last = 0;
-    setInterval(helper,50);
+    requestAnimationFrame(getMp);
 
-    function createBubbles(){
-        for(let i=0;i<500;i++){
+    function createBubbles() {
+        for (let i = 0; i < 500; i++) {
             arr[i] = {};
             arr[i].div = document.createElement('div');
-            let val = Math.floor(Math.random()*22);
-            arr[i].div.style.width=val+'px';
-            arr[i].div.style.height=val+'px';
+            let val = Math.floor(Math.random() * 22);
+            arr[i].div.style.width = val + 'px';
+            arr[i].div.style.height = val + 'px';
             arr[i].div.style.background = 'blue';
             arr[i].div.style.borderRadius = '50%';
             arr[i].div.style.position = 'absolute';
@@ -23,44 +23,46 @@ function main(){
             arr[i].posX = -1;
             arr[i].posY = -1;
             arr[i].use = false;
+            arr[i].t = Math.floor(Math.random() * 5) + 1;
+            arr[i].A = Math.floor(Math.random() * 7) + 1;
+            arr[i].cont = 0;
         }
     }
 
 
-    function doHarmonic(){
-        x = Math.floor(Math.random()*35);
-        t = Math.floor(Math.random()*x);
-        A = Math.floor(Math.random()*x+2);
-        return A * Math.sin(t);
+    function doHarmonic(ob) {
+        // x = Math.floor(Math.random() * 35);
+        return ob.A * Math.sin(ob.t + ob.cont);
     }
 
-    function doStuffsWithBubbles(mouseX,mouseY){
+    function doStuffsWithBubbles(mouseX, mouseY) {
         let ok = 0;
         let ok1 = 0;
-        for(let i=0;i<500;i++){
-            if((arr[i].posX < 0 || arr[i].posY < 0) && ok == 0 && mouseX >0 && mouseY >0){
+        for (let i = 0; i < 500; i++) {
+            if ((arr[i].posX < 0 || arr[i].posY < 0) && ok == 0 && mouseX > 0 && mouseY > 0) {
                 console.log('a');
                 arr[i].posX = mouseX;
                 arr[i].posY = mouseY;
-                arr[i].div.style.left = mouseX+'px';
-                arr[i].div.style.top = mouseY+'px';
+                arr[i].div.style.left = mouseX + 'px';
+                arr[i].div.style.top = mouseY + 'px';
                 document.getElementsByTagName('body')[0].appendChild(arr[i].div);
                 ok = 1;
-            }else{
-                arr[i].div.style.top = (arr[i].posY - 5) + 'px';
-                let val = doHarmonic();
+            } else {
+                arr[i].div.style.top = (arr[i].posY - 3) + 'px';
+                let val = doHarmonic(arr[i]);
                 arr[i].div.style.left = (arr[i].posX + val) + 'px';
-                arr[i].posY -= 5;
+                arr[i].cont += (Math.random()/5);
+                arr[i].posY -= 3;
                 arr[i].posX = arr[i].posX + val;
                 ok1 = 1;
             }
         }
-        // if(ok == 1 || ok1 == 1){
-        //     setTimeout(requestAnimationFrame(getMp),500);
-        // }
+        if (ok == 1 || ok1 == 1) {
+            requestAnimationFrame(getMp);
+        }
     }
 
-    document.addEventListener('mouseover',function(e){
+    document.addEventListener('mouseover', function (e) {
         posX = e.clientX;
         posY = e.clientY;
         //doStuffsWithBubbles(e.clientX,e.clientY);
@@ -73,7 +75,7 @@ function main(){
         // document.getElementsByTagName('body')[0].appendChild(arr[last]);
         // last++;
     });
-    document.addEventListener('mousemove',function(e){
+    document.addEventListener('mousemove', function (e) {
         posX = e.clientX;
         posY = e.clientY;
         //doStuffsWithBubbles(e.clientX,e.clientY);
@@ -86,16 +88,13 @@ function main(){
         // document.getElementsByTagName('body')[0].appendChild(arr[last]);
         // last++;
     });
-    document.addEventListener('mouseleave',function(e){
+    document.addEventListener('mouseleave', function (e) {
         posX = -1;
         posY = -1;
         console.log('left');
     });
-    function helper(){
-        requestAnimationFrame(getMp);
-    }
-    function getMp(){
-        doStuffsWithBubbles(posX,posY);
+    function getMp() {
+        doStuffsWithBubbles(posX, posY);
         //doStuffsWithBubbles(posX,posY);
         //doStuffsWithBubbles(posX,posY);
         //doStuffsWithBubbles(posX,posY);
