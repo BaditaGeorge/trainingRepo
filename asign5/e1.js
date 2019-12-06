6b2b941b991f6c752a3e79d3221736f8d34058e0
BaditaGeorge <baditageorge@yahoo.com>
2019-12-06 16:36:19 +0200

    assignment 5

--- asign5/e1.js
+++ asign5/e1.js
@@ -1,8 +1,7 @@
-let list_Of_positions = [{x:15,y:15,t:'s',l:50},{x:37,y:55,t:'s',l:48},{x:450,y:380,t:'c',l:35},{x:140,y:200,t:'s',l:50},{x:183,y:200,t:'s',l:43}]
+let list_Of_positions = [{x:15,y:15,t:'s',l:50},{x:37,y:55,t:'s',l:48},{x:450,y:380,t:'c',l:35},{x:470,y:400,t:'c',l:15},{x:140,y:200,t:'s',l:50},{x:183,y:200,t:'s',l:43}]
 
 function draw_Shapes(){
     for(let elem of list_Of_positions){
-        console.log(elem.x,elem.y);
         let x = document.createElement('div');
         x.style.width = elem.l + 'px';
         x.style.height = elem.l + 'px';
@@ -13,9 +12,16 @@ function draw_Shapes(){
         if(elem.t === 'c'){
             x.style.borderRadius = '50%';
         }
+        elem.ref = x;
         document.getElementsByTagName('body')[0].appendChild(x);
     }
 }
+
+function log_and_paint(el1,el2){
+    console.log(el1.x,el1.y,el2.x,el2.y,'Suprapunere');
+    el1.ref.style.backgroundColor = 'yellow'
+}
+
 function check_square_overlaping(){
     for(let i=0; i<list_Of_positions.length; i++){
         let el1 = list_Of_positions[i];
@@ -25,31 +31,31 @@ function check_square_overlaping(){
                 if(el2.t === 's'){
                     if(el1.x > el2.x && el1.y > el2.y){
                         if(el2.x + el2.l >= el1.x || el2.y + el2.l >= el1.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }
                     }else if(el1.x < el2.x && el1.y < el2.y){
                         if(el1.x + el1.l >= el2.x || el1.y + el1.l >= el2.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }
                     }else if(el1.x < el2.x && el1.y > el2.y){
                         if(el1.x + el1.l >= el2.x && el2.y + el2.l >= el1.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }
                     }else if(el1.x > el2.x && el1.y < el2.y){
                         if(el2.x + el2.l >= el1.x && el1.y + el1.l >= el2.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         } 
                     }else if(el1.x === el2.x){
                         if(el1.y > el2.y && el2.y + el2.l >= el1.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }else if(el1.y < el2.y && el1.y + el1.l >= el2.y){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }
                     }else if(el1.y === el2.y){
                         if(el1.x > el2.x && el2.x + el2.l >= el1.x){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }else if(el1.x < el2.x && el1.x + el1.l >= el2.x){
-                            console.log(i,j,'Suprapunere');
+                            log_and_paint(el1,el2);
                         }
                     }
                 }
@@ -58,5 +64,29 @@ function check_square_overlaping(){
     }
 }
 
+function check_circles_overlaping(){
+    for(let i=0; i<list_Of_positions.length; i++){
+        let el1 = list_Of_positions[i];
+        if(el1.t === 'c'){
+            for(let j=i+1; j<list_Of_positions.length; j++){
+                let el2 = list_Of_positions[j];
+                if(el2.t === 'c'){
+                    let middleX = (el1.x + (el1.l/2));
+                    let middleY = (el1.y + (el1.l/2));
+                    let middleX_2 = (el2.x + (el2.l/2));
+                    let middleY_2 = (el2.y + (el2.l/2));
+                    let distSqr = (middleX - middleX_2)*(middleX - middleX_2) + (middleY - middleY_2)*(middleY - middleY_2);
+                    let sumOfRadiuses = ((el1.l/2) + (el2.l/2))*((el1.l/2) + (el2.l/2));
+                    if(sumOfRadiuses > distSqr){
+                        log_and_paint(el1,el2);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 draw_Shapes();
 check_square_overlaping();
+check_circles_overlaping();
